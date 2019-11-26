a65d2bd817b61258b9b10eb8b0f19d49b35b2f04
cherylandjimmy <cherylandjimmy888@gmail.com>
2019-11-25 23:24:41 -0500

    ch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,14 +10,17 @@ export class AppComponent {
   // title = 'Cheryl and Jimmy';
   // displayQA = false;
   // genderReveal = false;
-
   constructor() { } 
-
+  optionDisplay:number = 0;
   displayPhotos:Boolean;
   genderAnswered($event) {
     this.genderAnswer = $event;
+    console.log($event);
+    this.optionDisplay = $event== "Boy" ? 2: 1;
   }
   receiveMessage($event) {
-    this.displayPhotos = $event
+    this.displayPhotos = $event;
+    console.log($event);
+    this.optionDisplay = $event == "Boy" ? 2: 1;
   }
 }
--- src/app/card-photos/card-photos.component.ts
+++ src/app/card-photos/card-photos.component.ts
@@ -9,8 +9,14 @@ export class CardPhotosComponent implements OnInit {
   data:object;
   list:object = [
     {src:'https://lh3.googleusercontent.com/26Db1Jh0EDTSIvkEK-c0qJehSsKmeAX65VQTSh1Be5YdeZfEtTVFIjlQDKEOhYG0IbHGil0MAvK90ZauiGOEQtHv4A3RUDV6x1FwzNeK7gFuEdAKZeI8He5zi-AXg9XLR_7_nwIet8Y=w1920-h1080', 
-    option: {one: 'Boy', two: 'Girl'}, title:'Jimmy And Cheryl', subtitle:"Guess the baby's gender?", text:'Is it a boy or a girl?'},
-    {src:'', option: {one: 'Next'}, title:"Gender Reveal", subtitle:"It's a boy!", 
+    option: {one: 'Boy', two: 'Girl'}, title:'Jimmy And Cheryl', subtitle:"Guess our baby's gender?", text:'Is it a boy or a girl?'},
+    
+    {src:'https://lh3.googleusercontent.com/7ZKbFIkqJ5iJEeUUYPJmLW-_LbptRq8Rf5a1hq-MmwMHzIGkt27vRgWuFskFbjXCxDoIUAbDTw-y4Yb9xaGsQM1ONx-Aa7eXARrcBLgluGxKgxcZqZ7P0q1iK15-W-q9hTmcvGcxir0=w1920-h1080',
+    option: {one: 'Boy', two: 'Girl'}, title:"Guess the our baby's gender?", subtitle:"Here's a hint", 
+    text:""},
+    
+    {src:'https://lh3.googleusercontent.com/7ZKbFIkqJ5iJEeUUYPJmLW-_LbptRq8Rf5a1hq-MmwMHzIGkt27vRgWuFskFbjXCxDoIUAbDTw-y4Yb9xaGsQM1ONx-Aa7eXARrcBLgluGxKgxcZqZ7P0q1iK15-W-q9hTmcvGcxir0=w1920-h1080', 
+    option: {one: 'Next'}, title:"Gender Reveal", subtitle:"It's a boy!", 
     text:"We are having a boy! Miles is currently 34 weeks old. He's now ready to accept your contributions!"},
   ];
   answer: string = '';
