e674a6db3d4d8a0ad2e33abc75236ceb1b88a0cc
cherylandjimmy <cherylandjimmy888@gmail.com>
2019-11-26 01:03:32 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,43 @@ export class AppComponent {
   // displayQA = false;
   // genderReveal = false;
   constructor() { } 
+  ready:boolean=false;
   optionDisplay:number = 0;
   displayPhotos:Boolean;
   genderAnswered($event) {
     this.genderAnswer = $event;
-    console.log($event);
-    this.optionDisplay = $event== "Boy" ? 2: 1;
+    console.log($event,this.genderAnswer );
+    if ($event =="Start Over"){
+      this.optionDisplay = 0;
+      this.ready = false;
+      this.genderAnswer = false;
+    }
+    else if ($event =="Skip")
+    {
+      this.optionDisplay = 2;
+      this.ready = true;
+      this.genderAnswer = "Boy";
+    }
+    else if ($event == "Ready")
+    {
+      this.ready=true; 
+      this.optionDisplay=1;
+    }
+    else if ($event =="Next")
+    {
+      this.optionDisplay = 3;
+    }
+    else
+      { 
+        this.ready=true;
+        this.optionDisplay = $event== "Boy" ? 2: 2;
+        console.log(   this.optionDisplay)
+    }
   }
   receiveMessage($event) {
+    console.log($event)
+    
     this.displayPhotos = $event;
-    console.log($event);
     this.optionDisplay = $event == "Boy" ? 2: 1;
-  }
+}
 }
--- src/app/card-photos/card-photos.component.ts
+++ src/app/card-photos/card-photos.component.ts
@@ -8,16 +8,21 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 export class CardPhotosComponent implements OnInit {
   data:object;
   list:object = [
-    {src:'https://lh3.googleusercontent.com/26Db1Jh0EDTSIvkEK-c0qJehSsKmeAX65VQTSh1Be5YdeZfEtTVFIjlQDKEOhYG0IbHGil0MAvK90ZauiGOEQtHv4A3RUDV6x1FwzNeK7gFuEdAKZeI8He5zi-AXg9XLR_7_nwIet8Y=w1920-h1080', 
-    option: {one: 'Boy', two: 'Girl'}, title:'Jimmy And Cheryl', subtitle:"Guess our baby's gender?", text:'Is it a boy or a girl?'},
+    {src:'https://lh3.googleusercontent.com/9oXXeJO8nzU8Q8lUwgsJA8ell6dGJRcQ7Tf7G7Si4CaXfhBB3U7jR8mxYhZ_EGCoELBMinkTchriBCI4Lkz9LrXCm20W_VJ0zAQcAZPcg-wh_2rYpbgLXGt1O3L4arX3M9hvZaI5qJFa1e83R_h5-qFZe43Vg0Cgax1CPBcwnzaPxjIUDaz6CYGGB-9oo_Asl_KFXfpOQHzj8FAGxFRP3HVvmh8ZXQejMaMjpbGmnmIN-Qv5ICOqvA-XvOINVLHxcRaWkOsZk_pnpa-sqzZ9Rkld1V44-ZqJLEKwy3E-x_JE8YItoPVj_ObuYNSxIsr-JFQBCBDfOohcLxBUUmWouNq4sB_3uHq7vLIucL3LglqSrxR4wUKwlwSRb7SgIgXslNwnwDOU_8O6K1cVitW1PPWljh94bKEE1bUuIINq6aRoOmRu3mw982d3D7N7u-4WeHWj9CGiVliwKWCc_duKq2r6_08xrahSDKvKjC-9F2AvqLdS6B3QoYm9ljFNFU9WfrVoIWFSKC1xFyQnJ9qvYoQovzYj2gMl2Vb2V_7IeZxiSwfu4RhdomyONhTJmEN6QkOOvX-hfSaZ0ZhMm9W2GIrAoAu7wPl2km7dZM6xTld-3kl4Rk1HAQTemjro4XcTF9GHzgumUn7aQZ4ejUufHWxmYdoN652oTklh86tHS8AEv5K-ArGZTm6R1qhK_mtX704IkymrsKadofXdJluJk_jRr___v7yZmqRBsoff8mZq_myh=w687-h915-no', 
+    option: {one: 'Ready', two:'Skip'}, title:'Jimmy And Cheryl', subtitle:"Guess our baby's gender?", text:'Are you ready?'},
     
     {src:'https://lh3.googleusercontent.com/7ZKbFIkqJ5iJEeUUYPJmLW-_LbptRq8Rf5a1hq-MmwMHzIGkt27vRgWuFskFbjXCxDoIUAbDTw-y4Yb9xaGsQM1ONx-Aa7eXARrcBLgluGxKgxcZqZ7P0q1iK15-W-q9hTmcvGcxir0=w1920-h1080',
-    option: {one: 'Boy', two: 'Girl'}, title:"Guess the our baby's gender?", subtitle:"Here's a hint", 
+    option: {one: 'Boy', two: 'Girl'}, title:"Guess our baby's gender?", subtitle:"Here's a hint", 
     text:""},
     
     {src:'https://lh3.googleusercontent.com/7ZKbFIkqJ5iJEeUUYPJmLW-_LbptRq8Rf5a1hq-MmwMHzIGkt27vRgWuFskFbjXCxDoIUAbDTw-y4Yb9xaGsQM1ONx-Aa7eXARrcBLgluGxKgxcZqZ7P0q1iK15-W-q9hTmcvGcxir0=w1920-h1080', 
     option: {one: 'Next'}, title:"Gender Reveal", subtitle:"It's a boy!", 
-    text:"We are having a boy! Miles is currently 34 weeks old. He's now ready to accept your contributions!"},
+    text:"We are having a boy! His name will be Miles and he is currently 34 weeks old."},
+
+    {src:'https://lh3.googleusercontent.com/26Db1Jh0EDTSIvkEK-c0qJehSsKmeAX65VQTSh1Be5YdeZfEtTVFIjlQDKEOhYG0IbHGil0MAvK90ZauiGOEQtHv4A3RUDV6x1FwzNeK7gFuEdAKZeI8He5zi-AXg9XLR_7_nwIet8Y=w1920-h1080', 
+     additionalOption:[{text:'Amazon', link:''},{text:'Buy Buy Baby', link:''}],
+     option: {one:'',two:'Start Over'}, title:'Miles is now ready to accept your contributions', subtitle:"", text:"Miles is now ready to accept your contributions! Thank you for your support!"},
+    
   ];
   answer: string = '';
   @Output() genderAnswered = new EventEmitter<boolean>();
