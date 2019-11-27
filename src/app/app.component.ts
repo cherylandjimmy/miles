11931594c269c96873d5ffcf0e81681dfda891b9
cherylandjimmy <cherylandjimmy888@gmail.com>
2019-11-26 21:31:43 -0500

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component  } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //import { ButtonThankYouComponent } from "./button-thank-you/button-thank-you.component";
 @Component({
   selector: 'app-root',
@@ -6,49 +6,49 @@ import { Component  } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  // title = 'Cheryl and Jimmy';
-  // displayQA = false;
-  // genderReveal = false;
-  constructor() { } 
-  ready:boolean=false;
-  optionDisplay:number = 0;
-  displayPhotos:Boolean;
-  genderAnswer:string;
+export class AppComponent implements OnInit {
+  ready: boolean = false;
+  optionDisplay: number = 0;
+  displayPhotos: Boolean;
+  genderAnswer: string;
+
+  ngOnInit() {
+    var v_stage = sessionStorage.getItem('ff');
+    if (v_stage == null) {
+      this.optionDisplay = 0;
+    }
+    else {
+      this.optionDisplay = Number(v_stage);
+    }
+  }
+
   genderAnswered($event) {
     this.genderAnswer = $event;
-    console.log($event,this.genderAnswer );
-    if ($event =="Start Over"){
+    if ($event == "Start Over") {
       this.optionDisplay = 0;
       this.ready = false;
       this.genderAnswer = '';
     }
-    else if ($event =="Skip")
-    {
+    else if ($event == "Skip") {
       this.optionDisplay = 2;
       this.ready = true;
       this.genderAnswer = "Boy";
     }
-    else if ($event == "Ready")
-    {
-      this.ready=true; 
-      this.optionDisplay=1;
+    else if ($event == "Ready") {
+      this.ready = true;
+      this.optionDisplay = 1;
     }
-    else if ($event =="Next")
-    {
+    else if ($event == "Next") {
+      sessionStorage.setItem('ff', '3');
       this.optionDisplay = 3;
     }
-    else
-      { 
-        this.ready=true;
-        this.optionDisplay = $event== "Boy" ? 2: 2;
-        console.log(   this.optionDisplay)
+    else {
+      this.ready = true;
+      this.optionDisplay = $event == "Boy" ? 2 : 2;
     }
   }
   receiveMessage($event) {
-    console.log($event)
-    
     this.displayPhotos = $event;
-    this.optionDisplay = $event == "Boy" ? 2: 1;
-}
+    this.optionDisplay = $event == "Boy" ? 2 : 1;
+  }
 }
